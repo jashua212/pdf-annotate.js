707605f84aaaf05295a01276bd7844679a9557be
jashua212 <jashua212@gmail.com>
2021-09-28 13:17:36 -0400

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,56 @@
-var webpack = require('webpack');
-var fileName = 'pdf-annotate';
+
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: './docs/main.js',
+	output: {
+		filename: 'index.js',
+		path: path.resolve(__dirname, 'docs') // output directory
+	}
+
+   /*  module: {
+		loaders: [{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015']
+				}
+			}
+		]
+	} */
+};
+
+
+/* var fileName = 'pdf-annotate';
 var plugins = [];
 
 if (process.env.MINIFY) {
-  fileName += '.min'
-  plugins.push(
-    new webpack.optimize.UglifyJsPlugin()
-  );
+	fileName += '.min'
+	plugins.push(
+		new webpack.optimize.UglifyJsPlugin());
 }
 
 module.exports = {
-  devtool: 'source-map',
-  plugins: plugins,
-  entry: './index.js',
-  output: {
-    filename: 'dist/' + fileName + '.js',
-    library: 'PDFAnnotate',
-    libraryTarget: 'umd'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015'],
-          plugins: ['add-module-exports']
-        }
-      }
-    ]
-  }
-};
+	devtool: 'source-map',
+	plugins: plugins,
+	entry: './index.js',
+	output: {
+		filename: 'dist/' + fileName + '.js',
+		library: 'PDFAnnotate',
+		libraryTarget: 'umd'
+	},
+	module: {
+		loaders: [{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015'],
+					plugins: ['add-module-exports']
+				}
+			}
+		]
+	}
+}; */
